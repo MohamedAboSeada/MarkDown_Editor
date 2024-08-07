42dde73e22373afa565b8b2fd69879f4dba95b3b
MohamedAboSeada <madoaxel287@gmail.com>
2024-08-07 10:37:43 +0300

    no changes

--- src/components/MarkdownPrev.jsx
+++ src/components/MarkdownPrev.jsx
@@ -4,8 +4,9 @@ import { ThemeContext } from './ThemeContext';
 import { Marked } from 'marked';
 import { markedHighlight } from 'marked-highlight';
 import hljs from 'highlight.js';
-import 'highlight.js/styles/atom-one-dark.css'; // Import the CSS style
+import 'highlight.js/styles/arta.css'; // Import the CSS style
 import { MathJax } from 'better-react-mathjax';
+
 const marked = new Marked(
 	markedHighlight({
 		langPrefix: 'hljs language-',
